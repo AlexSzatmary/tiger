a662412eb3a241871af57668cfc31507bf174926
Alex C. Szatmary <alex.szatmary@gmail.com>
2013-05-03 16:59:44 -0400

    Solve general PDE's, fix Robin BC

--- my_model.py
+++ my_model.py
@@ -39,11 +39,11 @@ class Robin(object):
         if self.side == 'left':
             return (2 * u[1] +
                     (-2 + 2 * dx * self.a / self.b) * u[0]
-                    - 2 * dx * self.c / self.b)
+                    - 2 * dx * self.c / self.b) / dx ** 2
         elif self.side == 'right':
             return (2 * u[-2] +
                     (-2 - 2 * dx * self.a / self.b) * u[-1]
-                    + 2 * dx * self.c / self.b)
+                    + 2 * dx * self.c / self.b) / dx ** 2
 
 
 class VonNeumann(Robin):
@@ -52,64 +52,85 @@ class VonNeumann(Robin):
 
 
 def opd2udx2(u, dx):
-    "Applies second-derivative finite difference operator on u"
+    """Applies second-derivative finite difference operator on u
+
+    Note that this takes some array, u, of length N, but returns second
+    derivatives for only nodes 2 through N-1"""
     return (u[0:-2] - 2. * u[1:-1] + u[2:]) / dx ** 2
 
 
-class Model1s(object):
+class PDESolver(object):
     """Time-varying reaction-diffusion type of solver
-    Solves dm/dt = D d2m/dx2 - c * m"""
-    def __init__(self, N=16, L=None, m0=None, lhs=None, rhs=None, D=None,
-                 dt=None, Nt=None, c=None):
+    Solves du/dt = f(u, du/dx, d2u/dx2)"""
+    def __init__(self, N=16, dudt=None, L=None, u0=None, lhs=None, rhs=None,
+                 dt=None, Nt=None, run=True):
         self.N = N
+        self.dudt = dudt
         self.L = L
         self.dx = L / (N - 1)
         self.x = np.arange(0, N) * self.dx
-        if issubclass(type(m0), numbers.Number):
-            self.m0 = np.ones(self.N) * m0
+        if issubclass(type(u0), numbers.Number):
+            self.u0 = np.ones(self.N) * u0
         else:
-            self.m0 = m0(self.x)
+            self.u0 = u0(self.x)
         self.lhs = lhs
         self.rhs = rhs
-        self.D = D
         self.dt = dt
         self.Nt = Nt
-        self.c = c
 
         self.t = np.arange(0, self.Nt) * self.dt
         if type(self.lhs) is Dirichlet:
-            self.m0[0] = self.lhs.u_0
+            self.u0[0] = self.lhs.u_0
         if type(self.rhs) is Dirichlet:
-            self.m0[-1] = self.rhs.u_0
+            self.u0[-1] = self.rhs.u_0
 
-    def odeint_wrapper(self):
-        self.m = scipy.integrate.odeint(
-            self.dmdt, self.m0, self.t,
+        if run:
+            self.run()
+
+    def run(self):
+        self.u = scipy.integrate.odeint(
+            self.d_dudt, self.u0, self.t,
             args=(self,))
 
     @staticmethod
-    def dmdt(m, t, self):
-        # Watch out! Because this is a staticmethod, as required by odeint,
-        # self is the third argument
-        r = np.zeros(np.size(m))
+    def d_dudt(u, t, self):
+        """"
+        Discretized application of dudt
+
+        Watch out! Because this is a staticmethod, as required by odeint, self
+        is the third argument
+        """
+        r = np.zeros(np.size(u))
 
         if type(self.lhs) is Dirichlet:
             r[0] = 0.
         else:
-            r[0] = self.D * self.lhs.opd2udx2(m, self.dx)
+            r[0] = self.dudt(u[0], self.lhs.opd2udx2(u, self.dx))
 
         if type(self.rhs) is Dirichlet:
             r[-1] = 0.
         else:
-            r[-1] = self.D * self.rhs.opd2udx2(m, self.dx)
+            r[-1] = self.dudt(u[-1], self.rhs.opd2udx2(u, self.dx))
+#            r[-1] = self.dudt(u[-1], opd2udx2(u[-3:], self.dx))
 
-        r[1:-1] = self.D * opd2udx2(m, self.dx) - self.c * m[1:-1]
+        r[1:-1] = self.dudt(u[1:-1], opd2udx2(u, self.dx))
         return r
 
 
-m1s = Model1s(N=16, L=1., m0=0.,
-              lhs=Dirichlet(1.), rhs=VonNeumann(dudx=0., side='right'), D=1.,
-              dt=0.01, Nt=1000, c=0.)
+class LinearPDESolver(PDESolver):
+    def __init__(self, D=None, a=None, c=None, **kwargs):
+        self.D = D
+        self.a = a
+        self.c = c
+        def dudt(u, upp):
+            "du/dt = D d2u/dx2 + a u + c"
+            return D * upp + a * u + c
+        super(LinearPDESolver, self).__init__(dudt=dudt, **kwargs)
+
+m1s = LinearPDESolver(N=16, L=1., u0=0.,
+                      lhs=Dirichlet(1.),
+                      rhs=VonNeumann(dudx=0., side='right'), D=1., a=0,
+                      dt=0.01, Nt=1000, c=0.)
 
 
 def main(argv=None):
